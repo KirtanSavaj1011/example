ee4bbeabbc9cb56efdb84402fff21fab7eadf877
KirtanSavaj1011 <kirtans.we3vision@gmail.com>
2025-12-22 12:27:32 +0530

    Fix: adjust dotenv for production

--- config/supabase.js
+++ config/supabase.js
@@ -1,18 +1,3 @@
-// const { createClient } = require('@supabase/supabase-js');
-// const dotenv = require('dotenv');
-
-// // Link the config.env file
-// dotenv.config({ path: './config.env' });
-
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
-// // Initialize the Supabase Client
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// module.exports = supabase;
-
-
 const { createClient } = require('@supabase/supabase-js');
 
 // Only try to load config.env if we are NOT on Render (local development)
